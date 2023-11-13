0891a3de494c7ee434062c4f1f80bda92bce94c1
Joshua Noel <noelejoshua@gmail.com>
2023-11-13 16:21:52 -0500

    overhauled the class

--- src/main/java/comp3607_group_project/AssignmentAnalyzer.java
+++ src/main/java/comp3607_group_project/AssignmentAnalyzer.java
@@ -1,5 +1,8 @@
 package comp3607_group_project;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.lang.Class;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -7,19 +10,37 @@ import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class AssignmentAnalyzer {
+import org.apache.commons.io.FileUtils;
+import org.javatuples.Triplet;
 
-    public Class<?> getInstance(String folder, String classname) {
-        try {
-            Class<?> c = Class.forName("comp3607_group_project." + folder + "." + classname); //create and return class object for manipulation
-            return c;
+public class AssignmentAnalyzer {  
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+    //get methods, constructos and attributes for a given java file
+    //get all class instances from assignment folder
+    public ArrayList<Class<?>> getInstances(String folder) {
+        ArrayList<Class<?>> classInstances = new ArrayList<Class<?>>();
+        File dir = new File(System.getProperty("user.dir") + "\\demo\\src\\main\\java\\comp3607_group_project\\" + folder); //looks for java files in the extracted zip folder
+		String[] extensions = new String[] { "java" };
+
+        List<File> files = (List<File>) FileUtils.listFiles(dir, extensions, true); //list of files with java extension in current directory
+
+        for (File file: files) {
+            try { //create class instance for each java file
+                Class<?> c = Class.forName("comp3607_group_project." + folder + "." + file.getName().replaceAll(".java", ""));
+                classInstances.add(c);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
         }
 
+        return classInstances;
     }
 
     public Dictionary<String, String> getPrivateAttrs(Class<?> c) {
@@ -55,7 +76,7 @@ public class AssignmentAnalyzer {
                 for (int j = 0; j < parameters.length; j++) {
                     if (parameters.length - 1 != j) {
                         signature = signature + parameters[j].getSimpleName();
-                        signature = signature + ", ";
+                        signature = signature + ",";
 
                     } else {
                         signature = signature + parameters[j].getSimpleName();
@@ -91,7 +112,7 @@ public class AssignmentAnalyzer {
                 for (int j = 0; j < parameters.length; j++) {
                     if (parameters.length - 1 != j) {
                         signature = signature + parameters[j].getSimpleName();
-                        signature = signature + ", ";
+                        signature = signature + ",";
 
                     } else {
                         signature = signature + parameters[j].getSimpleName();
@@ -111,5 +132,74 @@ public class AssignmentAnalyzer {
 
         return methodsDict;
     }
+
+    //get reuqired methods and attributes for entire assignment
+    public ArrayList<Triplet<String, String, Integer>> extractReqAttributes(String folder) {
+        ArrayList<Triplet<String, String, Integer>> attributes = new ArrayList<Triplet<String, String, Integer>>();
+        File file = new File(System.getProperty("user.dir") + "\\demo\\src\\main\\java\\comp3607_group_project\\" + folder + "\\formattedData.txt");
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file.getCanonicalPath()));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String [] tokens = line.split("\\s+"); //splits line into array of words
+                
+                if (tokens.length == 3) {
+                    if (!tokens[2].equals("MARKS")) { //ensures line is not table header
+                        if (!tokens[0].contains("(")) { //filters attributes from methods
+                            attributes.add(new Triplet<String, String, Integer>(tokens[0], tokens[1], Integer.parseInt(tokens[2])));
+                        }
+                    }
+
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return attributes;
+    }    
+
+    public ArrayList<Triplet<String, String, Integer>> extractReqMethods(String folder) {
+        ArrayList<Triplet<String, String, Integer>> methods = new ArrayList<Triplet<String, String, Integer>>();
+        File file = new File(System.getProperty("user.dir") + "\\demo\\src\\main\\java\\comp3607_group_project\\" + folder + "\\formattedData.txt");
+        Pattern pattern = Pattern.compile("[A-Za-z0-9]+\\([^)]*\\)", Pattern.CASE_INSENSITIVE);
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file.getCanonicalPath()));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String [] tokens = line.split("\\s+"); //splits line into array of words
+                
+                //normal method/attribute row
+                if (tokens.length == 3) {
+                    if (!tokens[2].equals("MARKS")) { //ensures line is not table header
+                        Matcher matcher = pattern.matcher(tokens[0]);
+
+                        if (matcher.matches()) { //filters methods from attributes
+                            methods.add(new Triplet<String, String, Integer>(tokens[0], tokens[1], Integer.parseInt(tokens[2])));
+                        }
+                    }
+                }
+
+                //constructor row
+                if (tokens.length == 2) {
+                    Matcher matcher = pattern.matcher(tokens[0]);
+
+                    if (matcher.matches()) { //filters methods from attributes
+                        methods.add(new Triplet<String, String, Integer>(tokens[0], null, Integer.parseInt(tokens[1])));
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return methods;
+    }   
 
 }
