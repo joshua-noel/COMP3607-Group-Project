fae43fc065a4c565f55e48f6d7a21a0b8eb9534d
Joshua Noel <noelejoshua@gmail.com>
2023-11-12 20:31:39 -0500

    added appendFolder method

--- src/main/java/comp3607_group_project/FileHandler.java
+++ src/main/java/comp3607_group_project/FileHandler.java
@@ -14,9 +14,10 @@ import java.util.List;
 import org.apache.commons.io.FileUtils;
 
 public class FileHandler {
+
     public void unzip(String folder) {
         String folderPath = System.getProperty("user.dir") + "\\demo\\src\\main\\resources\\" + folder + ".zip";
-        String destFolder = System.getProperty("user.dir") + "\\demo\\src\\main\\resources\\";//should override zip and replace it with unzipped ver
+        String destFolder = System.getProperty("user.dir") + "\\demo\\src\\main\\java\\comp3607_group_project\\"; //extracts to the area with all other program files
 
         try {
             ZipFile zipFile = new ZipFile(folderPath);
@@ -29,8 +30,52 @@ public class FileHandler {
 
     }
 
+    public void appendFiles(String folder) {
+        File dir = new File(System.getProperty("user.dir") + "\\demo\\src\\main\\java\\comp3607_group_project\\" + folder); //looks for java files in the extracted zip folder
+		String[] extensions = new String[] { "java" };
+
+        List<File> files = (List<File>) FileUtils.listFiles(dir, extensions, true); //list of files with java extension in current directory
+
+		for (File file : files) {
+            StringBuilder builder = new StringBuilder(); //new string builder obj for each file
+            String contents = "";
+
+            //get all file content
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file.getCanonicalPath()));
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    builder.append(line).append("\n"); //current line content
+
+                }
+
+                contents = builder.toString(); //final product
+                br.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                BufferedWriter bw = new BufferedWriter(new FileWriter(file.getCanonicalPath(), false));
+                //write the package name to the top of each file
+                bw.write("package comp3607_group_project." + folder + ";");
+                bw.newLine();
+                //write all file contents
+                bw.write(contents);
+                bw.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+			
+		}        
+
+    }
+
     private String getPdfPath(String folder) { //helper function
-        File dir = new File(System.getProperty("user.dir") + "\\demo\\src\\main\\resources\\" + folder); //looks for pdfs in the extracted zip folder
+        File dir = new File(System.getProperty("user.dir") + "\\demo\\src\\main\\java\\comp3607_group_project\\" + folder); //looks for pdfs in the extracted zip folder
 		String[] extensions = new String[] { "pdf" };
 
         List<File> files = (List<File>) FileUtils.listFiles(dir, extensions, true); //list of files with pdf extension in current directory
@@ -80,7 +125,7 @@ public class FileHandler {
 
     //Write data into a .txt document
     try {
-        FileWriter fw = new FileWriter(System.getProperty("user.dir") + "\\demo\\src\\main\\resources\\" + folder + "\\pdfText.txt");
+        FileWriter fw = new FileWriter(System.getProperty("user.dir") + "\\demo\\src\\main\\java\\comp3607_group_project\\" + folder + "\\pdfText.txt");
         fw.write(builder.toString());
         fw.flush();
         fw.close();
