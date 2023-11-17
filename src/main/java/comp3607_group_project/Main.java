54b19dce8bc675e3a8086d006f49519a8b491011
Joshua Noel <noelejoshua@gmail.com>
2023-11-16 21:26:52 -0400

    Add files via upload

--- src/main/java/comp3607_group_project/Main.java
+++ src/main/java/comp3607_group_project/Main.java
@@ -1,13 +1,25 @@
 package comp3607_group_project;
 
- 
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Running");
-        FileHandler handler = new FileHandler();
         ProcessFolderCommand processFolder = new ProcessFolderCommand("Joshua_Noel_816031055_A1");
+        ProcessPdfCommand processPdf = new ProcessPdfCommand("Joshua_Noel_816031055_A1");
         processFolder.execute();
-        handler.getRubicText("Joshua_Noel_816031055_A1");
+        processPdf.execute();
+
+        ProcessAssignmentCommand processAssignment = new ProcessAssignmentCommand("Joshua_Noel_816031055_A1");
+        processAssignment.execute();
+
+        TestSuite tester = new TestSuite("Joshua_Noel_816031055_A1");
+        tester.markAttributes();
+        tester.markConstructors();
+        tester.markMethods();
+        tester.computeTotalMarks();
+        System.out.println(tester.getAttrMarks());
+        System.out.println(tester.getConstructorMarks());
+        System.out.println(tester.getMethodMarks());
+        System.out.println(tester.getTotalMarks());
+
     }
 }
