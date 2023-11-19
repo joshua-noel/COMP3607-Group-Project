543a2261dba3e18ceb2865048bc584975478533f
alexapermel <150831484+alexapermel@users.noreply.github.com>
2023-11-19 19:02:27 -0400

    Update ProcessAssignmentCommand.java

--- src/main/java/comp3607_group_project/ProcessAssignmentCommand.java
+++ src/main/java/comp3607_group_project/ProcessAssignmentCommand.java
@@ -2,6 +2,10 @@ package comp3607_group_project;
 
 import java.io.IOException;
 
+/**
+* The ProcessAssignmentCommand class is responsible for executing a sequence of tasks,
+* including marking attributes, methods, constructors, and generating a PDF report.
+*/
 public class ProcessAssignmentCommand implements Command {
     private TestSuite tester;
 
