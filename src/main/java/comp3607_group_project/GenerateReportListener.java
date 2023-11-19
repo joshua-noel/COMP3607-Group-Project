7aae34bf5e9c0602caa24efdd1a7089e26bf04c9
alexapermel <150831484+alexapermel@users.noreply.github.com>
2023-11-19 18:55:49 -0400

    Update GenerateReportListener.java

--- src/main/java/comp3607_group_project/GenerateReportListener.java
+++ src/main/java/comp3607_group_project/GenerateReportListener.java
@@ -3,6 +3,9 @@ package comp3607_group_project;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+* ActionListener implementation for the "Generate Report" button in the GUIWindow class.
+*/
 public class GenerateReportListener implements ActionListener {
     private GUIWindow s;
     public GenerateReportListener(GUIWindow s){
