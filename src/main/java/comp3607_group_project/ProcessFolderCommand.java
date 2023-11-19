9cdefc193a455b24e94f1f1bd4debef15eb8512c
alexapermel <150831484+alexapermel@users.noreply.github.com>
2023-11-19 19:21:27 -0400

    Update ProcessFolderCommand.java

--- src/main/java/comp3607_group_project/ProcessFolderCommand.java
+++ src/main/java/comp3607_group_project/ProcessFolderCommand.java
@@ -1,7 +1,9 @@
 package comp3607_group_project;
 
- 
-
+/**
+* The ProcessFolderCommand class is responsible for executing tasks related to handling files in a folder,
+* including unzipping the folder and appending package information to Java files.
+*/
 public class ProcessFolderCommand implements Command {
     FileHandler handler = new FileHandler();
     private String folder;
