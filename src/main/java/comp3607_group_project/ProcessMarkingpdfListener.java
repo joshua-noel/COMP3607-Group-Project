4a21f8481e83944bd2040e343f56718bf81a335a
alexapermel <150831484+alexapermel@users.noreply.github.com>
2023-11-19 19:44:55 -0400

    Update ProcessMarkingpdfListener.java

--- src/main/java/comp3607_group_project/ProcessMarkingpdfListener.java
+++ src/main/java/comp3607_group_project/ProcessMarkingpdfListener.java
@@ -3,6 +3,10 @@ package comp3607_group_project;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+* The ProcessMarkingpdfListener class is responsible for triggering the execution of marking PDF processing tasks
+* when the associated button is clicked in the GUIWindow.
+*/
 public class ProcessMarkingpdfListener implements ActionListener {
     private GUIWindow s;
     
