6e96b126087516149c575204a5a3f8636f55ed9e
alexapermel <150831484+alexapermel@users.noreply.github.com>
2023-11-20 15:02:10 -0400

    javadoc GenerateReportListener.java

--- src/main/java/comp3607_group_project/GenerateReportListener.java
+++ src/main/java/comp3607_group_project/GenerateReportListener.java
@@ -7,11 +7,26 @@ import java.awt.event.ActionListener;
 * ActionListener implementation for the "Generate Report" button in the GUIWindow class.
 */
 public class GenerateReportListener implements ActionListener {
+
+    /**
+     * The GUIWindow instance associated with this listener.
+     */
     private GUIWindow s;
+
+     /**
+     * Constructs a GenerateReportListener with a reference to the GUIWindow instance.
+     *
+     * @param s The GUIWindow instance to associate with this listener.
+     */
     public GenerateReportListener(GUIWindow s){
         this.s= s;
     }
 
+    /**
+     * Invoked when the "Generate Report" button is clicked, triggering the generation of a report.
+     *
+     * @param e The ActionEvent representing the button click.
+     */
     public void actionPerformed(ActionEvent e){
         s.generateReport();
     }
