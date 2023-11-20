2688e4f9fcb65a9bdd240d12d34c3df0c569576f
alexapermel <150831484+alexapermel@users.noreply.github.com>
2023-11-20 15:42:31 -0400

    javadoc ProcessPdfCommand.java

--- src/main/java/comp3607_group_project/ProcessPdfCommand.java
+++ src/main/java/comp3607_group_project/ProcessPdfCommand.java
@@ -5,14 +5,29 @@ package comp3607_group_project;
 * including extracting rubric text and parsing the extracted text to generate formatted data.
 */
 public class ProcessPdfCommand implements Command{
+     /**
+     * The FileHandler instance used for handling file-related tasks.
+     */
     private FileHandler handler = new FileHandler();
+
+    /**
+     * The name of the folder associated with the marking PDF.
+     */
     private String folder;
 
+    /**
+     * Constructs a ProcessPdfCommand with the specified folder name.
+     *
+     * @param folder The name of the folder associated with the marking PDF.
+     */
     public ProcessPdfCommand(String folder) {
         this.folder = folder;
 
     }
 
+    /**
+     * Executes the command to extract rubric text and parse the extracted text to generate formatted data.
+     */
     public void execute() {
         handler.getRubicText(folder);
         handler.parseRubricText(folder);
