6129eb172d4a69bddd9f127d7b61c3cfbd497f5a
alexapermel <150831484+alexapermel@users.noreply.github.com>
2023-11-20 15:23:26 -0400

    javadoc Main.java

--- src/main/java/comp3607_group_project/Main.java
+++ src/main/java/comp3607_group_project/Main.java
@@ -1,6 +1,16 @@
 package comp3607_group_project;
 
+/**
+ * The Main class serves as the entry point for the application.
+ * It creates an instance of the GUIWindow class to initiate the user interface.
+ */
 public class Main {
+
+     /**
+     * The main method that creates an instance of the GUIWindow class.
+     *
+     * @param args The command-line arguments (not used in this application).
+     */
     public static void main(String[] args) {
         GUIWindow window= new GUIWindow();
     }
