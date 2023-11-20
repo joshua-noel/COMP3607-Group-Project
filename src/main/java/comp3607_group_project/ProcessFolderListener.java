3a6a064709a913d97385806ed3a02b0cb5ffcbf5
alexapermel <150831484+alexapermel@users.noreply.github.com>
2023-11-20 15:38:23 -0400

    jaadoc ProcessFolderListener.java

--- src/main/java/comp3607_group_project/ProcessFolderListener.java
+++ src/main/java/comp3607_group_project/ProcessFolderListener.java
@@ -7,11 +7,25 @@ import javax.swing.*;
 * when the associated button is clicked in the GUIWindow.
 */
 public class ProcessFolderListener implements ActionListener{
+     /**
+     * The GUIWindow instance associated with this listener.
+     */
     private GUIWindow s;
 
+    /**
+     * Constructs a ProcessFolderListener with the specified GUIWindow instance.
+     *
+     * @param s The GUIWindow instance associated with this listener.
+     */
     public ProcessFolderListener(GUIWindow s){
         this.s= s;
     }
+
+    /**
+     * Invoked when the associated button is clicked, triggering the execution of folder processing tasks.
+     *
+     * @param e The ActionEvent representing the button click.
+     */
     public void actionPerformed(ActionEvent e){
         s.processfolder();
     }
