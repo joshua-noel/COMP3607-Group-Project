ebc84006357ce12b18266f1e71fc3e3609aec4ed
alexapermel <150831484+alexapermel@users.noreply.github.com>
2023-11-20 15:10:46 -0400

    javadoc HelpbtnListener.java

--- src/main/java/comp3607_group_project/HelpbtnListener.java
+++ src/main/java/comp3607_group_project/HelpbtnListener.java
@@ -9,11 +9,26 @@ import javax.swing.*;
 
 
 public class HelpbtnListener implements ActionListener{
+
+    /**
+     * The GUIWindow instance associated with this listener.
+     */
     private GUIWindow s;
 
+    /**
+     * Constructs a HelpbtnListener with a reference to the GUIWindow instance.
+     *
+     * @param s The GUIWindow instance to associate with this listener.
+     */
     public HelpbtnListener(GUIWindow s){
         this.s= s;
     }
+
+    /**
+     * Invoked when the help button is clicked, triggering the display of help information.
+     *
+     * @param e The ActionEvent representing the button click.
+     */
     public void actionPerformed(ActionEvent e){
         s.help();
     }
