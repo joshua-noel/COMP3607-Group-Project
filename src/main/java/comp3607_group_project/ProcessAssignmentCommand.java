af8cfa8a8cd4188c4ba9a18f57df4c75ef5d4d5b
alexapermel <150831484+alexapermel@users.noreply.github.com>
2023-11-20 15:29:34 -0400

    javadoc ProcessAssignmentCommand.java

--- src/main/java/comp3607_group_project/ProcessAssignmentCommand.java
+++ src/main/java/comp3607_group_project/ProcessAssignmentCommand.java
@@ -7,13 +7,25 @@ import java.io.IOException;
 * including marking attributes, methods, constructors, and generating a PDF report.
 */
 public class ProcessAssignmentCommand implements Command {
+    /**
+     * The TestSuite instance used for marking attributes, methods, constructors, and generating a PDF report.
+     */
     private TestSuite tester;
 
+    /**
+     * Constructs a ProcessAssignmentCommand with the specified folder.
+     *
+     * @param folder The name of the folder containing the assignment files.
+     */
     public ProcessAssignmentCommand(String folder) {
         tester = new TestSuite(folder);
 
     }
 
+     /**
+     * Executes the command to mark attributes, methods, constructors, and generate a PDF report.
+     * Handles IOException if any of the tasks encounters an I/O error.
+     */
     public void execute() {
         try {
             tester.markAttributes();
