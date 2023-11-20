a97388499470b5fb76b2e0a4bbe6b68412388415
SomanthaWestfield <151282340+SomanthaWestfield@users.noreply.github.com>
2023-11-19 21:26:01 -0400

    Update HelpbtnListener.java

--- src/main/java/comp3607_group_project/HelpbtnListener.java
+++ src/main/java/comp3607_group_project/HelpbtnListener.java
@@ -1,5 +1,11 @@
 package comp3607_group_project;
 
+/**
+ * The HelpbtnListener class implements the ActionListener to respond to user interaction
+ * specifically designed to handle help button events in a GUIWindow.
+ */
+
+
 public class HelpbtnListener implements ActionListener{
     private GUIWindow s;
 
