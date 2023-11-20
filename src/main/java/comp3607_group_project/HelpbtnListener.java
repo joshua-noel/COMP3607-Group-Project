f29b48c2aa3550a603831dea30ee234860551e67
kasim taylor <kasimt15@gmail.com>
2023-11-19 20:55:24 -0400

    adding functionality to help button

---
+++ src/main/java/comp3607_group_project/HelpbtnListener.java
@@ -0,0 +1,12 @@
+package comp3607_group_project;
+
+public class HelpbtnListener implements ActionListener{
+    private GUIWindow s;
+
+    public GUIWindow(GUIWindow s){
+        this.s= s;
+    }
+    private void actionPerformed(ActionEvent e){
+
+    }
+}
