f1ca2db916c6bd8438a8500f76c5afbdca8ad318
alexapermel <150831484+alexapermel@users.noreply.github.com>
2023-11-20 15:34:21 -0400

    javadoc ProcessFolderCommand.java

--- src/main/java/comp3607_group_project/ProcessFolderCommand.java
+++ src/main/java/comp3607_group_project/ProcessFolderCommand.java
@@ -5,14 +5,29 @@ package comp3607_group_project;
 * including unzipping the folder and appending package information to Java files.
 */
 public class ProcessFolderCommand implements Command {
+    /**
+     * The FileHandler instance used for handling file-related tasks.
+     */
     FileHandler handler = new FileHandler();
+
+    /**
+     * The name of the folder to be processed.
+     */
     private String folder;
 
+    /**
+     * Constructs a ProcessFolderCommand with the specified folder name.
+     *
+     * @param folder The name of the folder to be processed.
+     */
     public ProcessFolderCommand(String folder) {
         this.folder = folder;
 
     }
 
+    /**
+     * Executes the command to unzip the folder and append package information to Java files.
+     */
     public void execute() {
         handler.unzip(folder);
         handler.appendFiles(folder);
